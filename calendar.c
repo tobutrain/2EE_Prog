9d20d273b56d90a48927ef77db6f3c6182eb140c
tobutore <tobutrain_10000@yahoo.co.jp>
2022-07-26 23:49:12 +0900

    コメント追加

--- calendar.c
+++ calendar.c
@@ -13,6 +13,7 @@ int main(void){
         printf("月: "); scanf("%d",&month); //表示月を取得
         
         //曜日を判定する
+        //入力-1年までの閏年を変数leapにカウントする
         int leap = 0; //
         for(i=1;i<year;i++){
             if((year%4==0 && year%100!=0) || year%400==0){
@@ -20,46 +21,54 @@ int main(void){
             }
         }
 
+        //(入力-1)月までの日数を変数mdaysumにカウントする
         int mdaysum = 0;
         for (i=0;i<month-1;i++){
             mdaysum += date[i];
         }
 
-        int thisleap = 0;
+        //今年が閏年かを判定する(変数thisLeap; 0-閏年でない/1-閏年)
+        int thisLeap = 0;
         if((year%4==0 && year%100!=0) || year%400==0){
-            thisleap = 1;
+            thisLeap = 1;
         }
 
+        //西暦1年1月1日から当月の1日までの経過日数を変数daysumに計算する
         int daysum = (year-1)*365+mdaysum+1+leap;
 
+        //もし入力月が2月より後で且つ今年が閏年ならば，変数daysumをインクリメントする
         if(month>2){
-            if(thisleap==1){
+            if(thisLeap==1){
                 daysum++;
             }
         }
 
+        //カレンダーのインデントを計算するため，経過日数の7の剰余を変数indentに代入する
         int indent = daysum % 7;
 
 
         //カレンダーを表示する
         system("cls");
-        printf("%d年%d月\n\n",year,month);
-        puts(" Sun Mon Tue Wed Thu Fri Sat");
+        printf("%d年%d月\n\n",year,month); //"<入力年>年<入力月>月"と表示
+        puts(" Sun Mon Tue Wed Thu Fri Sat"); //見出しを表示
         
+        //計算したインデントの数だけ空白を出力
         for(i=1;i<=indent;i++){
             printf("    ");
         }
 
-        int j = indent;
+        int j = indent; //この後のカウントアップに用いるため，変数jにインデントの値を代入しておく
 
-        int dateA = date[month-1];
+        int dateA = date[month-1]; //月の日数を代入した配列dateから，当月の日数を取得する
 
-        if((year%4==0 && year%100!=0) || year%400==0){
+        //もし当年が閏年でかつ当月が2月なら，取得した日付をインクリメントする
+        if(thisLeap==1){
             if(month==2){
                 dateA++;
             }
         }
 
+        //7つ出力したら改行しつつ，取得した日付までカウントアップして出力する
         for(i=1;i<=dateA;i++){
             printf("%4d",i);
             j++;
@@ -68,6 +77,8 @@ int main(void){
                 j = 0;
             }
         }
+
+        //改行コード／プログラム一時停止
         printf("\n\n");
         system("PAUSE");
 
