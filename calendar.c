48e522ba27cfaad30332f0abc737cd35be1b578f
tobutore <tobutrain_10000@yahoo.co.jp>
2022-07-26 23:12:14 +0900

    22/7/25授業

--- calendar.c
+++ calendar.c
@@ -4,16 +4,16 @@
 int main(void){
 
     while(1) {
-        system("cls");
+        system("cls"); //コンソールの画面をクリアする
     
-        int year,month,i;
-        int date[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+        int year,month,i; //int型のyear,month,iを宣言する
+        int date[12] = {31,28,31,30,31,30,31,31,30,31,30,31}; //int型の配列dateを宣言し，後述の内容で初期化する
         
-        printf("年: "); scanf("%d",&year); //表示年の入力
-        printf("月: "); scanf("%d",&month); //表示月の入力
+        printf("年: "); scanf("%d",&year); //表示年を取得
+        printf("月: "); scanf("%d",&month); //表示月を取得
         
         //曜日を判定する
-        int leap = 0;
+        int leap = 0; //
         for(i=1;i<year;i++){
             if((year%4==0 && year%100!=0) || year%400==0){
             leap++;
---
+++ work5-12.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main(void){
+    int tensu[2][4][3] = {{{91,63,78},{67,72,46},{89,34,53},{32,54,34}},{{97,67,82},{73,43,46},{97,56,21},{85,46,35}}};
+    int sum[4][3];
+
+    //2回分の点数の合計を求める
+    
+
+    //点数表示
+    for(int h = 0;h<2;h++){
+        for(int i = 0;i<4;i++){
+            for(int j = 0;j<3;j++){
+                sum[i][j] += tensu[h][i][j];
+            }
+        }
+
+        printf("%d回目の点数\n",h+1);
+        for(int i = 0;i<4;i++){
+            for(int j = 0;j<3;j++){
+                printf("%4d",tensu[h][i][j]);
+            }
+            putchar('\n');
+        }
+    }
+
+    //合計点を表示
+    puts("合計点");
+    for(int i = 0;i<4;i++){
+        for(int j = 0;j<3;j++){
+            printf("%4d",sum[i][j]);
+        }
+        putchar('\n');
+    }
+
+    return 0;
+}
+
