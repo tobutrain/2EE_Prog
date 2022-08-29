7d2393ce24a6fea314167bbe50bfb409fc4026bc
tobutore <contact@tobutrain.net>
2022-08-29 18:50:32 +0900

    modify: guchi-2

--- calendar-guchi.c
+++ calendar-guchi.c
@@ -2,13 +2,8 @@
 
 int main(void)
  {
-	int year;   /*年の変数*/
-	int month;  /*月の変数*/
+	int year,month,dow,py,sum,i;   /*各変数の宣言*/
 	int day[13] = {0,31,28,31,30,31,30,31,31,30,31,30,30};   /*各月の日数*/
-    int dow;    /*曜日の計算に使用する変数*/
-    int py;     /*前年(Previous year)の変数*/
-    int sum;    /*日数の合計を示す変数*/
-    int i;
     int cnt = 0; /*改行用のカウントに使用する変数cnt*/
 
 	printf("年:");		/*年の入力*/
@@ -22,10 +17,12 @@ int main(void)
     py = year - 1; /*前年をpyに格納する*/
     sum = 365 * py + py / 4 + py / 400 - py / 100; 
     /*日数の計算をする。前年までの日数＋閏年の分の日数*/
-        for (i = 1; i < month; i++) /*iの値がmonth以下である限り続ける*/
-            sum += day[i]; /*変数sumにday[i]を加えたものを新しい変数sumとして格納する*/
-            sum++;  /*変数sumに１を加えた結果を代入する演算子*/
-            dow = sum % 7; /*曜日を確定させるために変数sumを７で割る。その余りを曜日番号とする*/
+    for (i = 1; i < month; i++) /*iの値がmonth以下である限り続ける*/
+        sum += day[i]; /*変数sumにday[i]を加えたものを新しい変数sumとして格納する*/
+    sum++;  /*変数sumに１を加えた結果を代入する演算子*/
+    dow = sum % 7; /*曜日を確定させるために変数sumを７で割る。その余りを曜日番号とする*/
+
+    printf("\n%d年%d月\n",year,month);
 
     printf(" Sun Mon Tue Wed Thu Fri Sat \n"); /*曜日の名前*/
     for(i = 0; i < dow; i++) /*iの値がdow以下である限り続ける*/
